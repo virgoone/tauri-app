5340017a41477e119b4d61f84831921ee7e2224f
virgoone <me@virgo.one>
2022-07-12 17:08:06 +0800

    fix: add menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,36 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let menu_instance = menu_instance();
+
+    tauri::Builder::default()
+        .menu(menu_instance)
+        .on_menu_event(|event| match event.menu_item_id() {
+            "quit" => {
+                std::process::exit(0);
+            }
+            "close" => {
+                event.window().close().unwrap();
+            }
+            _ => {}
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+fn menu_instance() -> Menu {
+    let quit = CustomMenuItem::new("quit".to_string(), "退出");
+    let close = CustomMenuItem::new("close".to_string(), "关闭");
+    let submenu = Submenu::new("文件", Menu::new().add_item(quit).add_item(close));
+
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "隐藏"))
+        .add_submenu(submenu);
+
+    return menu;
 }
